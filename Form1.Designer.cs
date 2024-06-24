94f13ccd172d3c9345a0983a2d4d7834cec59244
kuskas112 <vasileniyaivanka@mail.ru>
2024-06-24 20:39:36 +0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SummerPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paintButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // paintButton
+            // 
+            this.paintButton.Location = new System.Drawing.Point(12, 74);
+            this.paintButton.Name = "paintButton";
+            this.paintButton.Size = new System.Drawing.Size(95, 44);
+            this.paintButton.TabIndex = 0;
+            this.paintButton.Text = "Paint";
+            this.paintButton.UseVisualStyleBackColor = true;
+            this.paintButton.Click += new System.EventHandler(this.paintButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "y = ";
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(57, 24);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(212, 26);
+            this.input.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 530);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.paintButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button paintButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox input;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq.Expressions;
+using System.Diagnostics;
+
+namespace SummerPractice
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Point center;
+        int scale = 20;
+        double gap = 0.5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void drawXLine()
+        {
+            Pen pen; 
+            int count = (center.X) / scale;
+            for (int i = 0; i < count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    pen = Pens.Black;
+                }
+                else
+                {
+                    pen = Pens.Red;
+                }
+
+                g.DrawLine(pen, center.X + scale * i, center.Y, center.X + scale * (i+1), center.Y);
+                g.DrawLine(pen, center.X + scale * -i, center.Y, center.X + scale * (-i-1), center.Y);
+            }
+        }
+
+        public void drawYLine()
+        {
+            Pen pen;
+            int count = (center.Y) / scale;
+            for (int i = 0; i < count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    pen = Pens.Black;
+                }
+                else
+                {
+                    pen = Pens.Red;
+                }
+
+                g.DrawLine(pen, center.X, center.Y + scale * i, center.X, center.Y + scale * (i+1));
+                g.DrawLine(pen, center.X, center.Y + scale * -i, center.X, center.Y + scale * (-i-1));
+            }
+        }
+
+        private List<Point> calculateFunction()
+        {
+            List<Point> points = new List<Point>();
+            string func = input.Text;
+            int count = center.X / scale;
+            for (double x = -count; x <= count; x += gap)
+            {
+                string expr = func.Replace("x", "(" + x.ToString() + ")");
+                expr = expr.Replace(",", ".");
+                double result = 0;
+                try
+                {
+                    result = Convert.ToDouble(new DataTable().Compute(expr, ""));
+                }
+                catch (DivideByZeroException ex){
+                    result = double.NegativeInfinity;
+                }
+                points.Add(new Point(center.X + (int)(x * scale), center.Y - (int)(result * scale)));
+            }
+            return points;
+        }
+
+        private void showResult()
+        {
+            List<Point> points = calculateFunction();
+            int cnt = 0;
+            foreach (var point in points)
+            {
+                cnt ++;
+                if (point.Y < 0)
+                {
+                    continue;
+                }
+                drawPoint(point);
+            }
+
+            for (int i = 1; i < points.Count; i++)
+            {
+                if (points[i].Y < 0 || points[i-1].Y < 0)
+                {
+                    continue;
+                }
+                g.DrawLine(Pens.Aqua, points[i - 1], points[i]);
+            }
+        }
+
+        private void drawPoint(Point point)
+        {
+            if (point.Y < 0) { return; }
+            g.FillEllipse(Brushes.Red, point.X-2, point.Y-2, 4, 4);
+        }
+
+        private void paintButton_Click(object sender, EventArgs e)
+        {
+            g = CreateGraphics();
+            g.Clear(DefaultBackColor);
+            center = new Point(this.Width / 2, this.Height / 2);
+            drawXLine();
+            drawYLine();
+            showResult();
+        }
+    }
+}
